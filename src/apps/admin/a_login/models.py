16e4bac7406c8d653b7e4e3a56ebf00fc5ec4603
Xianyu Meng <254382M@student.gla.ac.uk>
2021-02-01 21:32:58 +0800

    Reupload

--- src/apps/admin/a_login/models.py
+++ src/apps/admin/a_login/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 # Create your models here.
-########
+########test####################
+class admin_account(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=32)
+    telephone = models.CharField(max_length=50,null= True)
+    location = models.CharField(max_length=500,null= True)
+    money = models.CharField(max_length=100,null= True)
