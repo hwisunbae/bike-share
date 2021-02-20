3c1edead4bdadaca75c9af98ecdbb5a70c3aad51
Hwisun Bae <hwisunbae.hb@gmail.com>
2021-02-20 14:42:17 +0000

    conflict solved

--- src/apps/operator/o_index/views.py
+++ src/apps/operator/o_index/views.py
@@ -4,14 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    # if get POST request
-    if request.POST:
-       pass
-    # the first request, just show the pages
-    else:
-        # saveAccount()
+    userid = request.COOKIES.get("o_userid")
+    if userid:
         context = {}
-        context['login'] = 'hello'
         return render(request, 'operator/o_index.html', context)
-
-
+    else:
+        return render(request, 'operator/o_login.html')
