2d3cbd412979334cec5e8f437dff18c57d7e02cc
Hwisun Bae <hwisunbae.hb@gmail.com>
2021-02-21 01:24:25 +0000

    csrf used in all over operator page and start-time and end-time DB store fixed

--- src/apps/operator/o_manage_bike/views.py
+++ src/apps/operator/o_manage_bike/views.py
@@ -9,23 +9,22 @@ from django.views.decorators.csrf import csrf_exempt
 from a_manage_bike.models import *
 from o_manage_bike.models import *
 
-@csrf_exempt
-def findBike(request):
-    if request.GET:
-        pass
-    else:
-        context = {}
-        context['login'] = 'hello'
-        return render(request, 'operator/o_find_bike.html',context)
 
-@csrf_exempt
-def repairBike(request):
-    if request.GET:
-        pass
-    else:
+def findBike(request):
+    userid = request.COOKIES.get("o_userid")
+    if userid:
         context = {}
-        context['login'] = 'hello'
+        return render(request, 'operator/o_find_bike.html', context)
+    else:
+        return render(request, 'operator/o_login.html')
+
+def repairBike(request):
+    userid = request.COOKIES.get("o_userid")
+    if userid:
+        context = {}
         return render(request, 'operator/o_repair_bike.html', context)
+    else:
+        return render(request, 'operator/o_login.html')
 
 @csrf_exempt
 def repairBike_do(request):
@@ -53,16 +52,18 @@ def addNewRepair_do(request):
 
         bikeId = request.POST.get('bikeId')
         location = request.POST.get('location')
-        startTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        endTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        new_end_time = datetime.datetime.strptime(endTime, '%Y-%m-%d %H:%M:%S')
+        startTime = request.POST.get('timeStart')
         new_start_time = datetime.datetime.strptime(startTime, '%Y-%m-%d %H:%M:%S')
         bikeid = bike.objects.get(id=bikeId)
         operatorid = operator_account.objects.get(id=request.COOKIES.get("o_userid"))
 
         obj = operator_repair_history(
-            # enddate
             operator_id=operatorid,
             bike_id=bikeid,
             location=location,
+            end_time=new_end_time,
             start_time=new_start_time
         )
         obj.save()
@@ -70,13 +71,10 @@ def addNewRepair_do(request):
     else:
         return HttpResponse("error")
 
-@csrf_exempt
 def transportBike(request):
-    if request.GET:
-        pass
-    else:
+    userid = request.COOKIES.get("o_userid")
+    if userid:
         context = {}
-        context['login'] = 'hello'
-        return render(request, 'operator/o_transport_bike.html',context)
-
-
+        return render(request, 'operator/o_transport_bike.html', context)
+    else:
+        return render(request, 'operator/o_login.html')
