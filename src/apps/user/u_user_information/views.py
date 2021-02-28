33fe7e052b3f24173b99fbcc64194212ecc85aa5
Mrwu123 <39589471+Mrwu123@users.noreply.github.com>
2021-02-28 00:22:45 +0000

    add user recharge change information

--- src/apps/user/u_user_information/views.py
+++ src/apps/user/u_user_information/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from u_login.models import *
 # Create your views here.
 
 
 def index(request):
-    if request.GET:
-        pass
-    else:
+    userid = request.COOKIES.get("u_userid")
+    if userid:
         context = {}
-        context['login'] = 'hello'
-        return render(request,'user/u_user_information.html',context)
+        user = user_account.objects.get(id = userid)
+        context["user"] = user
+        return render(request, 'user/u_user_information.html', context)
+    else:
+        return render(request, 'user/u_login.html')
